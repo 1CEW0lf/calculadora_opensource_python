8446cf72d0178268050cb189c05be5fbc6d8b571
1CEW0lf <1CEW0lf>
2024-11-05 22:05:48 -0600

    menu y funciones de multiplicar y dividir, corrección de errores

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from funciones_calculadora import sumar_n_numeros
+from funciones_calculadora import multiplicacion_n_numeros
+from funciones_calculadora import division_2_numeros
 
 while True:
     print('''
@@ -13,9 +15,21 @@ Bienvenido a mi calculadora, por favor ingresa la opción que desees.
 ''')
     
     opcion = int(input('> '))
+    
     if opcion == 0:
         break
 
     elif opcion == 1:
-        resultado = sumar_n_numeros
+        resultado = sumar_n_numeros()  # Llamar a la función
         print(f'El resultado de tu suma es {resultado}')
+
+    elif opcion == 2:
+        resultado = multiplicacion_n_numeros()  # Llamar a la función
+        print(f'El resultado de tu multiplicación es {resultado}')
+
+    elif opcion == 3:
+        resultado = division_2_numeros()  # Llamar a la función
+        if resultado is not None:  # Comprobar si la división fue válida
+            print(f'El resultado de tu división es {resultado}')
+        else:
+            print("No se puede realizar la división.")
