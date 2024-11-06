0a1efa2c4788c8bbace9314c3ab1c4ac554cea00
1CEW0lf <1CEW0lf>
2024-11-05 21:15:25 -0600

    saludo

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hola mundo, pero ahora desde Linux")
+while True:
+    print('''
+Bienvenido a mi calculadora, por favor ingresa la opción que desees.
+--------------------------------------------------------------------
+          
+1) Hacer una suma de N números
+2) Hacer una multiplicación de N números
+3) Hacer una división de 2 números
+
+0) Salir del programa
+''')
+    
+    opcion = int(input('> '))
+    if opcion == 0:
+        break
